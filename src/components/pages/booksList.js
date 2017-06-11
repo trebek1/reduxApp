9278c43dc97ad3f51bec5268bac4dfea9391524d
Alex Trzeciak <trebek08@gmail.com>
2017-06-10 23:59:01 -0700

    used react bootstrap to style the app a bit

--- src/components/pages/booksList.js
+++ src/components/pages/booksList.js
@@ -4,11 +4,23 @@ import React from "react";
 import {connect} from 'react-redux'; 
 import {bindActionCreators} from "redux";
 import {getBooks} from "../../actions/booksActions"; 
+import {Grid, Col, Row, Button} from "react-bootstrap"; 
 
 class BooksList extends React.Component{
 
 	componentDidMount(){
-		this.props.getBooks(); 
+		this.props.getBooks([{
+			id: 4, 
+			title: "this is the book title 4", 
+			description: "this is the book description 4",
+			price: 542
+		},
+		{
+			id: 5, 
+			title: "this is the book title 5 ", 
+			description: "this is the book description 5",
+			price: 234
+		}]); 
 	}
 
 	render(){
@@ -18,15 +30,18 @@ class BooksList extends React.Component{
 					<h2>{booksArr.title}</h2>
 					<h2>{booksArr.description} </h2>
 					<h2>{booksArr.price}</h2>
+					<Button bsStyle="primary">Buy Now</Button>
+
 				</div>				
 			)
 		});
 
 		return(
-			<div>
-				<h1> Hello React </h1>
-				{bookList}
-			</div>
+			<Grid>
+				<Row style={{marginTop: "15px" }}>
+					{bookList}
+				</Row>
+			</Grid>
 		)
 	}
 
